605baad49285becab63a7628c9235fa97329bbbe
Adam Graham <adam@zigurous.com>
2023-06-18 12:13:49 -0500

    Add DoubleRange

---
+++ Editor/Structs/DoubleRangePropertyDrawer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Zigurous.Architecture.Editor
+{
+    [CustomPropertyDrawer(typeof(DoubleRange))]
+    public class DoubleRangePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            int indentLevel = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            position = DoubleField(position, property.FindPropertyRelative("m_Min"));
+            position = DoubleField(position, property.FindPropertyRelative("m_Max"));
+
+            EditorGUI.indentLevel = indentLevel;
+            EditorGUI.EndProperty();
+        }
+
+        private Rect DoubleField(Rect position, SerializedProperty property)
+        {
+            Rect field = EditorGUIUtility.GetFieldRect(position, 2);
+            position.x += field.width + EditorGUIUtility.standardHorizontalSpacing;
+
+            EditorGUI.BeginChangeCheck();
+
+            double value = EditorGUIUtility.FieldWrapper(property.displayName, (label) => {
+                return EditorGUI.DoubleField(field, label, property.doubleValue);
+            });
+
+            if (EditorGUI.EndChangeCheck()) {
+                property.doubleValue = value;
+            }
+
+            return position;
+        }
+
+    }
+
+}
--- Runtime/Extensions/ComparableExtensions.cs
+++ Runtime/Extensions/ComparableExtensions.cs
@@ -17,7 +17,8 @@ namespace Zigurous.Architecture
         /// <param name="includeMin">The minimum value is inclusive if true, exclusive if false.</param>
         /// <param name="includeMax">The maximum value is inclusive if true, exclusive if false.</param>
         /// <returns>True if the value is between the min and max.</returns>
-        public static bool IsBetween<T>(this T value, T min, T max, bool includeMin, bool includeMax) where T : IComparable<T>
+        public static bool IsBetween<T>(this T value, T min, T max, bool includeMin, bool includeMax)
+            where T : IComparable<T>
         {
             int minCompare = value.CompareTo(min);
             int maxCompare = value.CompareTo(max);
