c7f438674b43f81204dcdb73e30fcc6d2a1d61c4
Adam Graham <adam@zigurous.com>
2024-08-22 13:37:10 -0500

    Add CoroutineRunner singleton behavior

---
+++ Runtime/Behaviors/CoroutineRunner.cs
@@ -0,0 +1,10 @@
+namespace Zigurous.Architecture
+{
+    /// <summary>
+    /// A persistent singleton behavior that runs coroutines.
+    /// </summary>
+    public class CoroutineRunner : PersistentSingletonBehaviour<CoroutineRunner>
+    {
+    }
+
+}
