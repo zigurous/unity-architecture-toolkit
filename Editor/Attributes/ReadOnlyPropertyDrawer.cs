50d71ff665ea241705010f538447ed9bc189e17b
Adam Graham <adam@zigurous.com>
2025-03-07 05:27:40 -0600

    Fix incorrect height of expandable read only properties

--- Editor/Attributes/ReadOnlyPropertyDrawer.cs
+++ Editor/Attributes/ReadOnlyPropertyDrawer.cs
@@ -14,6 +14,11 @@ namespace Zigurous.Architecture.Editor
             }
         }
 
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
     }
 
 }
