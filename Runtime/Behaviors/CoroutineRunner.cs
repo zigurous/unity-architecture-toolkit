a63944858b85aaccd03144fbcfa8c8d70c0b91be
Adam Graham <adam@zigurous.com>
2025-04-05 04:53:05 -0500

    Set CoroutineRunner docs URL

--- Runtime/Behaviors/CoroutineRunner.cs
+++ Runtime/Behaviors/CoroutineRunner.cs
@@ -1,9 +1,13 @@
+using UnityEngine;
+
 namespace Zigurous.Architecture
 {
     /// <summary>
-    /// A persistent singleton behavior that runs coroutines.
+    /// A persistent singleton behavior that can be used to run coroutines.
     /// </summary>
-    public class CoroutineRunner : PersistentSingletonBehaviour<CoroutineRunner>
+    [AddComponentMenu("")]
+    [HelpURL("https://docs.zigurous.com/com.zigurous.architecture/api/Zigurous.Architecture/CoroutineRunner")]
+    public sealed class CoroutineRunner : PersistentSingletonBehaviour<CoroutineRunner>
     {
     }
 
