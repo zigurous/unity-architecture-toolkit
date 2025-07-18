0f405ab16fa6b9b172baa8e9af6985c6c1ce9adb
Adam Graham <adam@zigurous.com>
2025-07-18 13:39:29 -0500

    Add functions to generate random seeds from current time

--- Runtime/Utilities/HashCode.cs
+++ Runtime/Utilities/HashCode.cs
@@ -13,13 +13,7 @@ namespace Zigurous.Architecture
         /// <returns>The combined hash code.</returns>
         public static int Combine(int hash1, int hash2)
         {
-            unchecked // Overflow is fine, just wrap
-            {
-                int hash = 17;
-                hash = hash * 31 + hash1;
-                hash = hash * 31 + hash2;
-                return hash;
-            }
+            return System.HashCode.Combine(hash1, hash2);
         }
 
         /// <summary>
@@ -31,14 +25,7 @@ namespace Zigurous.Architecture
         /// <returns>The combined hash code.</returns>
         public static int Combine(int hash1, int hash2, int hash3)
         {
-            unchecked // Overflow is fine, just wrap
-            {
-                int hash = 17;
-                hash = hash * 31 + hash1;
-                hash = hash * 31 + hash2;
-                hash = hash * 31 + hash3;
-                return hash;
-            }
+            return System.HashCode.Combine(hash1, hash2, hash3);
         }
 
         /// <summary>
@@ -51,15 +38,28 @@ namespace Zigurous.Architecture
         /// <returns>The combined hash code.</returns>
         public static int Combine(int hash1, int hash2, int hash3, int hash4)
         {
-            unchecked // Overflow is fine, just wrap
-            {
-                int hash = 17;
-                hash = hash * 31 + hash1;
-                hash = hash * 31 + hash2;
-                hash = hash * 31 + hash3;
-                hash = hash * 31 + hash4;
-                return hash;
-            }
+            return System.HashCode.Combine(hash1, hash2, hash3, hash4);
+        }
+
+        /// <summary>
+        /// Generates a random seed by combining the current time into a hash code.
+        /// </summary>
+        /// <returns>A random seed as an integer.</returns>
+        public static int RandomSeed()
+        {
+            System.DateTime now = System.DateTime.UtcNow;
+            return Combine(now.Hour, now.Minute, now.Second, now.Millisecond);
+        }
+
+        /// <summary>
+        /// Generates a random seed by combining the current time into a hash
+        /// code and converting it to an unsigned integer.
+        /// </summary>
+        /// <returns>A random seed as an unsigned integer.</returns>
+        public static uint RandomUnsignedSeed()
+        {
+            long lseed = System.Convert.ToInt64(RandomSeed());
+            return unchecked(System.Convert.ToUInt32(lseed + int.MaxValue));
         }
 
     }
